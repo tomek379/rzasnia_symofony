30a1c832107664d406ee1d49e22c0273375d427c
Tomasz Bilski <tomekbilski6123@interia.pl>
2020-07-29 18:45:29 +0200

    Koniec etapu pierwszego

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -15,10 +15,66 @@ class ArticleController extends Controller{
         return $this->render('articles/index.html.twig');
     }
     /**
-    * @Route("/informacje/kapelmistrz")
+    * @Route("/galeria")
+    * @Method({"GET"})
+    */
+    public function galeria(){
+        return $this->render('articles/galeria.html.twig');
+    }
+    /**
+    * @Route("/galeria_yt")
+    * @Method({"GET"})
+    */
+    public function galeria_yt(){
+        return $this->render('articles/galeria_yt.html.twig');
+    }
+    /**
+    * @Route("/repertuar")
+    * @Method({"GET"})
+    */
+    public function repertuar(){
+        return $this->render('articles/repertuar.html.twig');
+    }
+    /**
+    * @Route("/posluchaj_nas")
+    * @Method({"GET"})
+    */
+    public function posluchaj_nas(){
+        return $this->render('articles/posluchaj_nas.html.twig');
+    }
+    /**
+    * @Route("/informacje")
+    * @Method({"GET"})
+    */
+    public function informacje(){
+        return $this->render('articles/informacje.html.twig');
+    }
+    /**
+    * @Route("/osiagniecia")
+    * @Method({"GET"})
+    */
+    public function osiagniecia(){
+        return $this->render('articles/osiagniecia.html.twig');
+    }
+    /**
+    * @Route("/czlonkowie")
+    * @Method({"GET"})
+    */
+    public function czlonkowie(){
+        return $this->render('articles/czlonkowie.html.twig');
+    }
+    /**
+    * @Route("/kapelmistrz")
     * @Method({"GET"})
     */
     public function kapelmistrz(){
         return $this->render('articles/kapelmistrz.html.twig');
+    }
+    /**
+    * @Route("/kontakt")
+    * @Method({"GET"})
+    */
+    public function kontakt(){
+        return $this->render('articles/kontakt.html.twig');
     }
 }
